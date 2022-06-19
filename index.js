e357fc055de81f807bbac6e13a49eadd2b278d80
Zacharia <ndunguzachary24@gmail.com>
2022-06-19 15:38:09 +0300

    commit tree 665cb254480bc0c55fc505a8ac9a8ee460e0387c

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      return false;
+    }
+
+    // if we reach the middle, and all the letters match, return true
+  }
+  return true;
 }
 
 /* 
@@ -12,9 +20,28 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
